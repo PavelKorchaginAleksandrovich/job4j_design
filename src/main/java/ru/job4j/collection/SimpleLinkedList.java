09f808a9093e4d8a9c47d8f260e51fc984b8bad7
Pavel <pav.intense@gmail.com>
2022-11-13 00:49:07 +0300

    2. Создать контейнер на базе связанного списка [#159 #360256]

--- src/main/java/ru/job4j/collection/SimpleLinkedList.java
+++ src/main/java/ru/job4j/collection/SimpleLinkedList.java
@@ -1,10 +1,7 @@
 package ru.job4j.collection;
 
 
-import java.util.ConcurrentModificationException;
-import java.util.Iterator;
-import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 public class SimpleLinkedList<E> implements LinkedList<E> {
 
@@ -16,20 +13,20 @@ public class SimpleLinkedList<E> implements LinkedList<E> {
     @Override
     public void add(E value) {
         Node<E> newLink = new Node<>(value);
-
-        if (null == this.first) {
-           this.first = newLink;
+        if (null == first) {
+           first = newLink;
+            last = newLink;
         } else {
-            this.last.next = newLink;
+            last.next = newLink;
+            last = newLink;
         }
-        this.last = newLink;
-        this.size++;
-        this.modCount++;
+        size++;
+        modCount++;
      }
 
     @Override
     public E get(int index) {
-        checkIndex(index);
+        Objects.checkIndex(index, size);
         Node<E> result = this.first;
         for (int i = 0; i < index; i++) {
             result = result.next;
@@ -49,32 +46,24 @@ public class SimpleLinkedList<E> implements LinkedList<E> {
     }
 
     private class Itr implements Iterator<E> {
-        private int expectedModCount = modCount;
+        private final int  expectedModCount = modCount;
         Node<E> current = first;
-        int cursor;
 
         @Override
         public boolean hasNext() {
-            return this.cursor < size;
-        }
-
-        @Override
-        public E next() {
-            checkForModification();
-            if (hasNext()) {
-                E result;
-                result = current.date;
-                this.current = this.current.next;
-                this.cursor++;
-                return result;
-            } else {
-                throw new NoSuchElementException();
-            }
-        }
-        final void checkForModification() throws ConcurrentModificationException {
-            if (modCount != this.expectedModCount) {
+            if (modCount != expectedModCount) {
                 throw new ConcurrentModificationException();
             }
+            return current != null;
+        }
+        @Override
+        public E next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            E result = current.date;
+            current = current.next;
+            return result;
         }
     }
     private static class Node<E> {
