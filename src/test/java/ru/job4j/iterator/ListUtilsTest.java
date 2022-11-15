a9b1fd8bcc8787f4804d1255935ffa82427beb9c
Pavel <pav.intense@gmail.com>
2022-11-15 10:55:17 +0300

    7. ListIterator [#350217 #360925]

--- src/test/java/ru/job4j/iterator/ListUtilsTest.java
+++ src/test/java/ru/job4j/iterator/ListUtilsTest.java
@@ -32,4 +32,25 @@ class ListUtilsTest {
         ListUtils.addAfter(input, 0, 2);
         assertThat(input).hasSize(3).containsSequence(1, 2, 3);
     }
+
+    @Test
+    void whenRemoveIf() {
+        ListUtils.addBefore(input, 1, 2);
+        ListUtils.removeIf(input, n -> n > 2);
+        assertThat(input).hasSize(2).containsSequence(1, 2);
+    }
+
+    @Test
+    void whenReplaceIf() {
+        ListUtils.addBefore(input, 1, 2);
+        ListUtils.replaceIf(input, n -> n == 3, 5);
+        assertThat(input).hasSize(3).containsSequence(1, 2, 5);
+    }
+
+    @Test
+    void whenRemoveAll() {
+        ListUtils.addBefore(input, 1, 2);
+        input.removeAll(List.of(1, 3));
+        assertThat(input).hasSize(1).containsSequence(2);
+    }
 }
