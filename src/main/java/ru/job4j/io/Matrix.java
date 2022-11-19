c886d53497a45c4cd9f79e877915fd1b33d8911b
Pavel <pav.intense@gmail.com>
2022-11-19 23:19:32 +0300

    8. Реализовать собственную структуру данных - HashMap [#1008]

---
+++ src/main/java/ru/job4j/io/Matrix.java
@@ -0,0 +1,36 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class Matrix {
+    public static void main(String[] args) {
+        String matrix = convertMatrix(getMatrix(10));
+        try {
+            FileOutputStream out = new FileOutputStream("matrix.txt");
+            out.write(matrix.getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    private static String convertMatrix(int[][] matrix) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                result.append(matrix[i][j]).append(" ");
+            }
+            result.append(System.lineSeparator());
+        }
+        return result.toString();
+    }
+
+    private static int[][] getMatrix(int size) {
+        int[][] matrix = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return matrix;
+    }
+}
