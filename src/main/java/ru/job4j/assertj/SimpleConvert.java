816e6fd27c0d5f23757f5229b8154cf92a093875
Pavel <pav.intense@gmail.com>
2022-11-20 01:46:53 +0300

    Утверждения с коллекциями [#504885 #357671]

--- src/main/java/ru/job4j/assertj/SimpleConvert.java
+++ src/main/java/ru/job4j/assertj/SimpleConvert.java
@@ -4,7 +4,6 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class SimpleConvert {
-
     public String[] toArray(String... example) {
         return example;
     }
