58276e60408200d2074fd5c255d3cf27cfdec52d
Pavel <pav.intense@gmail.com>
2022-12-21 19:41:01 +0300

    4. Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,7 +9,10 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-    Path start = Paths.get(".");
+    if (args.length == 0) {
+        throw new IllegalArgumentException("");
+    }
+    Path start = Paths.get(args[0]);
     search(start, p -> p.toFile().getName().endsWith(".java")).forEach(System.out::println);
     }
 
