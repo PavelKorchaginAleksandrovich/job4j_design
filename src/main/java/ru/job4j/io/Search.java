b02b56c5aac409095392e124aa05c7335c1c285d
Pavel <pav.intense@gmail.com>
2023-08-06 01:08:23 +0300

    12. Валидация параметров запуска [#246865 #434504]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,7 +12,7 @@ public class Search {
     public static void main(String[] args) throws IOException {
         checkArgs(args);
         Path start = Paths.get(args[0]);
-        search(start, p -> p.toFile().getName().endsWith(String.format(".%s", args[1]))).forEach(System.out::println);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) {
@@ -23,9 +24,24 @@ public class Search {
         }
         return searcher.getPaths();
     }
+
     private static void checkArgs(String[] args) {
         if (args.length < 2) {
             throw new IllegalArgumentException("Wrong counts of arguments");
         }
+        File file = new File(args[0]);
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException("File extension must start with a dot");
+        }
+        if (args[1].length() < 2) {
+            throw new IllegalArgumentException("File extension must be longer than 2");
+        }
     }
 }
+
