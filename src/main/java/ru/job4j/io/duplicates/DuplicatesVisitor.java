99d2beb1d0864f84ce525c5b68c3e04699389a4b
Pavel <pav.intense@gmail.com>
2023-08-06 01:12:46 +0300

    11. Поиск дубликатов [#315066 #366640]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -8,30 +8,25 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    private Set<FileProperty> duplicates = new HashSet<>();
-    private List<FileProperty> properties = new LinkedList<>();
-    private Map<String, FileProperty> propertiesMap = new HashMap<String, FileProperty>();
+    private Map<FileProperty, List<Path>> propertiesMap = new HashMap<>();
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        if (file.toFile().isFile()) {
-            FileProperty property = new FileProperty(file.toFile().length(), file.getFileName().toString());
-            file.toString();
-            if (propertiesMap.containsValue(property)) {
-                duplicates.add(property);
-             }
-            propertiesMap.put(file.toString(), property);
-        }
+         FileProperty property = new FileProperty(file.toFile().length(), file.getFileName().toString());
+
+         if (propertiesMap.containsKey(property)) {
+             propertiesMap.get(property).add(file);
+         } else {
+             propertiesMap.put(property, new LinkedList<>(List.of(file)));
+         }
+
         return super.visitFile(file, attrs);
     }
     public List<String> getDuplicates() {
         List<String> result = new LinkedList<>();
 
-        List<FileProperty> propertiesResult = new LinkedList<>(properties);
-        propertiesResult.retainAll(duplicates);
-        propertiesResult.forEach(p -> result.add(p.getName()));
         propertiesMap.forEach((k, v) -> {
-            if (duplicates.contains(v)) {
-                result.add(k);
+            if (v.size() > 1) {
+                v.forEach(path -> result.add(path.toString()));
             }
         });
         return result;
