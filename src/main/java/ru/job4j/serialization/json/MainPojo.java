73b7ead88455408a583c1d008b8e401ed3b11613
pavel <pav.intense@gmail.com>
2023-08-20 08:27:19 +0300

    5. Преобразование JSON в POJO. JsonObject [#315064]

--- src/main/java/ru/job4j/serialization/json/MainPojo.java
+++ src/main/java/ru/job4j/serialization/json/MainPojo.java
@@ -5,7 +5,7 @@ import org.json.JSONObject;
 public class MainPojo {
     public static void main(String[] args) {
         Document doc = new Document("passport", "1312", "456789");
-        Patient patient = new Patient("Andy", true, "andy@mail.ru andy@gmail.com".split(" "), 21 ,doc);
+        Patient patient = new Patient("Andy", true, "andy@mail.ru andy@gmail.com".split(" "), 21, doc);
         System.out.println(new JSONObject(patient));
         String stringPatient = "{\"emails\":[\"andy@mail.ru\",\"andy@gmail.com\"],\"document\":{\"number\":\"456789\",\"series\":\"1312\",\"type\":\"passport\"},\"name\":\"Andy\",\"male\":true,\"age\":21}";
         JSONObject jsonPatient = new JSONObject(stringPatient);
